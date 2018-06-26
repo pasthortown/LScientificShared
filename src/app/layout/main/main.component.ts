fef34f3308af0f1b78684ce23564a72f77a98773
pasthortown <pastorchessmaster@gmail.com>
2018-06-26 10:31:58 -0500

    solo mio

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,6 +1,9 @@
+import { Usuario } from './../../entidades/CRUD/Usuario';
+import { Documento } from './../../entidades/CRUD/Documento';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Http } from '@angular/http';
 import { saveAs } from "file-saver/FileSaver";
+import { UsuarioDocumento } from '../../entidades/CRUD/UsuarioDocumento';
 
 @Component({
   selector: 'app-main',
@@ -9,19 +12,20 @@ import { saveAs } from "file-saver/FileSaver";
 })
 export class MainComponent implements OnInit {
   @ViewChild('fileInput') fileInput;
-  pdfSrc: string;
-  latexcode: string;
   webServiceURL: string;
+  pdfSrc: string;
   pdfBytes: string;
   pagina: number;
-  tituloDocumento: string;
-  username: string;
+  documento: Documento;
+  usuario: Usuario;
   options: any = {printMargin: true, enableBasicAutocompletion: false, autoScrollEditorIntoView: true};
 
   constructor(public http: Http) { }
 
   ngOnInit() {
-    this.username = 'Luis Alfonso Salazar Vaca';
+    this.usuario = JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+    this.documento = new Documento();
+    this.documento.fecha = new Date();
     this.pagina = 1;
     this.pdfSrc = './../../../assets/pdfs/prueba.pdf';
     this.webServiceURL = 'http://192.168.20.10/LScientificShared/server/pdfbuilder/';
@@ -36,14 +40,14 @@ export class MainComponent implements OnInit {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
       reader.onload = (e) => {
-        this.latexcode = reader.result;
+        this.documento.contenido = reader.result;
       };
       reader.readAsText(file);
     }
   }
 
   Compilar() {
-    const data = {latex: this.latexcode};
+    const data = {latex: this.documento.contenido};
     this.http.post(this.webServiceURL + 'compilar', JSON.stringify(data))
     .subscribe(r1 => {
         const respuesta = r1.json();
@@ -62,13 +66,13 @@ export class MainComponent implements OnInit {
     }
     const byteArray = new Uint8Array(byteNumbers);
     const blob = new Blob([byteArray], { type: 'application/pdf' });
-    saveAs(blob, this.tituloDocumento);
+    saveAs(blob, this.documento.nombre);
   }
 
   DescargarArchivoTEX() {
-    const content = this.latexcode;
+    const content = this.documento.contenido;
     const blob = new Blob([ content ], { type : 'text/plain' });
-    saveAs(blob, this.tituloDocumento + '.tex');
+    saveAs(blob, this.documento.nombre + '.tex');
   }
 
   cambiarPagina(pagina: number) {
@@ -76,5 +80,53 @@ export class MainComponent implements OnInit {
         pagina = 1;
     }
     this.pagina = pagina;
+  }
+
+  guardarDocumentoBDD() {
+    this.http.get(this.webServiceURL + 'documento/leer_filtrado?columna=nombre&tipo_filtro=coincide&filtro=' + this.documento.nombre + '&idUsuario=' + this.usuario.id)
+    .subscribe(r => {
+      if ( r.json()[0] === 0 ) {
+        this.http.post(this.webServiceURL + 'documento/crear', JSON.stringify(this.documento))
+        .subscribe(r1 => {
+          this.http.get(this.webServiceURL + 'documento/leer_filtrado?columna=nombre&tipo_filtro=coincide&filtro=' + this.documento.nombre + '&idUsuario=' + this.usuario.id)
+          .subscribe(r2 => {
+            const documentoCreado = r2.json()[0] as Documento;
+            const documentoUsuario: UsuarioDocumento = new UsuarioDocumento();
+            documentoUsuario.idDocumento = documentoCreado.id;
+            documentoUsuario.idUsuario = this.usuario.id;
+            this.http.post(this.webServiceURL + 'usuariodocumento/crear', JSON.stringify(documentoUsuario))
+            .subscribe(r3 => {
+
+            }, error => {
+            });
+          }, error => {
+          });
+        }, error => {
+        });
+      } else {
+        const documentoPrevio = r.json()[0] as Documento;
+        this.documento.id = documentoPrevio.id;
+        this.http.post(this.webServiceURL + 'documento/actualizar', JSON.stringify(this.documento))
+        .subscribe(r1 => {
+        }, error => {
+        });
+      }
+    }, error => {
+    });
+  }
+
+  sincronizarDocumentoBDD() {
+    this.http.get(this.webServiceURL + 'documento/leer_filtrado?columna=nombre&tipo_filtro=coincide&filtro=' + this.documento.nombre + '&idUsuario=' + this.usuario.id)
+    .subscribe(r => {
+      if ( r.json()[0] === 0 ) {
+        return;
+      }
+      this.documento = r.json()[0] as Documento;
+    }, error => {
+    });
+  }
+
+  compartirDocumento() {
+
   }
 }
